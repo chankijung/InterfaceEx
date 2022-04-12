aceb07186c3a5bc0ca0195f521dac6bebe19ce02
chanki <chanki9507@gmail.com>
2022-04-12 11:29:15 +0900

    begin

---
+++ InterfaceEx/src/interfaceEx/Main.java
@@ -0,0 +1,27 @@
+package interfaceEx;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int num;
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("인사관리 프로그램");
+		System.out.println("1. 직원 관리");
+		System.out.println("2. 학생 관리");
+		System.out.println("3. 게시판 관리");
+		num = input.nextInt();
+		
+		switch(num) {
+		case 1 : //직원관리 기능 추가(등록, 조회, 검색, 삭제)
+			break;
+		case 2 : //학생관리 기능 추가(등록, 조회, 검색, 삭제)
+			break;
+		case 3 : //게시판관리 기능 추가(등록, 조회, 검색, 삭제)
+			break;
+		}
+	}
+
+}
