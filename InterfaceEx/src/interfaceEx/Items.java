d78637e54d0710637626cf53e474f334cbf5b3e3
chanki <chanki9507@gmail.com>
2022-04-12 14:03:23 +0900

    itmes 추가

---
+++ InterfaceEx/src/interfaceEx/Items.java
@@ -0,0 +1,71 @@
+package interfaceEx;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Items {
+	public void menu() {
+
+		Scanner input = new Scanner(System.in);
+		ArrayList<String> it = new ArrayList<String>();
+		String item;
+		while(true) {
+			int num;
+			System.out.println("상품 관리 페이지입니다");
+			System.out.println("1. 상품 등록");
+			System.out.println("2. 상품 조회");
+			System.out.println("3. 상품 검색");
+			System.out.println("4. 상품 삭제");
+			num = input.nextInt();
+			switch(num) {
+			case 1 :
+				System.out.println("등록할 상품을 입력해주세요");
+				item = input.next();
+				
+				if(it.contains(item)!= true) {
+					it.add(item);
+				}else {
+					System.out.println("이미 존재하는 상품입니다.");
+				}
+				break;
+			case 2 :
+				System.out.println("상품 목록 조회");
+				for(int i =0;i<it.size();i++) {
+					System.out.println(it.get(i));
+				}
+				break;
+			case 3 :
+				System.out.println("검색할 상품 입력 : ");
+				item= input.next();
+				int i;
+				for( i =0; i<it.size();i++) {
+					
+					if(item.equals(it.get(i))){
+						System.out.println("존재하는 상품입니다");
+						break;
+					}
+				}
+				if( i == it.size()) {
+					System.out.println("존재하지 않습니다");
+				}break;
+				
+			case 4 :
+				System.out.println("삭제할 상품 입력 :");
+				String name = input.next();
+				if(it.contains(name)== true) {
+					it.remove(name);
+					System.out.println(name+"이 삭제됐습니다");
+				}else {
+					System.out.println("입력한 상품이 존재하지 않습니다");
+				}
+				break;
+			}
+		
+	}
+	
+	}
+	
+	}
+	
+
+
--- InterfaceEx/src/interfaceEx/Main.java
+++ InterfaceEx/src/interfaceEx/Main.java
@@ -12,14 +12,23 @@ public class Main {
 		System.out.println("1. 직원 관리");
 		System.out.println("2. 학생 관리");
 		System.out.println("3. 게시판 관리");
+		System.out.println("4. 상품 관리");
 		num = input.nextInt();
 		
 		switch(num) {
-		case 1 : //직원관리 기능 추가(등록, 조회, 검색, 삭제)
+		case 1 : Member m = new Member();
+				 m.menu();
+				 //직원관리 기능 추가(등록, 조회, 검색, 삭제)
 			break;
-		case 2 : //학생관리 기능 추가(등록, 조회, 검색, 삭제)
+		case 2 :
+			//학생관리 기능 추가(등록, 조회, 검색, 삭제)
 			break;
-		case 3 : //게시판관리 기능 추가(등록, 조회, 검색, 삭제)
+		case 3 : 
+			//게시판관리 기능 추가(등록, 조회, 검색, 삭제)
+			break;
+		case 4 :Items i = new Items();
+				i.menu();
+			// 상품관리 기능 추가(등록, 조회, 검색, 삭제)
 			break;
 		}
 	}
